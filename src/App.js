35f8a3654140baff8791a0ded2dbb6d4683662de
susumu tomita <oyster880@gmail.com>
2023-05-13 07:30:27 +0900

    add list 3-17

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
-import Rect from "./Rect";
 import "./App.css";
 
 class App extends Component {
+  msgStyle = {
+    fontSize: "24px",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "Hello Component.",
+    };
+  }
+
   render() {
     return (
       <div >
         <h1>React</h1>
-        <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
-        <Rect x="150" y="100" w="150" h="150" c="magenta" r="75"/>
-        <Rect x="100" y="150" w="150" h="150" c="black" r="25"/>
+        <p style={this.msgStyle}>{this.state.msg}</p>
+        <p style={this.msgStyle}>{this.props.msg}</p>
       </div>
     );
   }
