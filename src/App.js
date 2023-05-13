915219126fce19b951be5bd7f39d98f73cb115fb
susumu tomita <oyster880@gmail.com>
2023-05-14 07:31:23 +0900

    add list 3-20,21

--- src/App.js
+++ src/App.js
@@ -9,17 +9,27 @@ class App extends Component {
     padding: "5px",
     borderBottom: "2px solid #900"
   };
+  btnStyle = {
+    fontSize: "20px",
+    padding: "0px 10px",
+    color: "#900",
+    margin: "20px 0px",
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      msg: "Hello Component.",
+      counter: 0,
+      msg: "count start!",
     };
-    let timer = setInterval(() => {
-      this.setState((state) => ({
-        msg: state.msg + "!"
-      }));
-    },1000);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: "count: " + state.counter,
+    }));
   }
 
   render() {
@@ -27,7 +37,7 @@ class App extends Component {
       <div >
         <h1>React</h1>
         <p style={this.msgStyle}>{this.state.msg}</p>
-        <p style={this.msgStyle}>{this.props.msg}</p>
+        <button style={this.btnStyle} onClick={this.doAction}>  Click</button>
       </div>
     );
   }
