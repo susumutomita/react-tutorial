db57fac945548e98074a2718e696492cc5c66db0
susumu tomita <oyster880@gmail.com>
2023-05-15 09:19:00 +0900

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders React title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders show list text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/show list./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders sample list title', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/サンプル・リスト/i);
+  expect(linkElement).toBeInTheDocument();
+});
