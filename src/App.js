5f8d30287f78fcb76fbf941502df5fa80d861baa
susumu tomita <oyster880@gmail.com>
2023-05-21 06:21:59 +0900

    add list3-32

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
 import React, { Component } from "react";
 import "./App.css";
 
-let data = {
-  title: "Title",
-  message: "this is sample message."
+let theme = {
+  light: {
+    backgroundColor: "#eef",
+    color: "#006",
+    padding: "10px",
+  },
+  dark: {
+    backgroundColor: "#006",
+    color: "#eef",
+    padding: "10px",
+  }
 };
-const SampleContext = React.createContext(data);
+
+const ThemeContext = React.createContext(theme.light);
 
 class App extends Component {
 
-  newdata = {
-    title: "新しいタイトル",
-    message: "これは新しいメッセージです。"
-  };
+  static contextType = ThemeContext;
   render() {
-    return <div>
-      <h1>Context</h1>
-      <Title />
-      <Message />
-      <SampleContext.Provider value={this.newdata}>
-        <Title />
-        <Message />
-      </SampleContext.Provider>
-      <Title />
-      <Message />
+    return <div style={this.context}>
+      <Title value="Content page" />
+      <Message value="This is Content sample." />
+      <Message value="※これはテーマのサンプルです。" />
     </div>;
   }
 }
 
 class Title extends Component {
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render() {
     return <div>
-      <h2>{this.context.title}</h2>
+      <h2 style={this.context}>{this.props.value}</h2>
     </div>;
   }
 }
 
 class Message extends Component {
 
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render() {
-    return <div>
-      <p>{this.context.message}</p>
-    </div>;
+    return (
+      <p style={this.context}>{this.props.value}</p>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders original title and message', () => {
+test('renders title and messages', () => {
   render(<App />);
-  const originalTitle = screen.getAllByText(/Title/i);
-  const originalMessage = screen.getAllByText(/this is sample message./i);
+  const title = screen.getByText(/Content page/i);
+  const message1 = screen.getByText(/This is Content sample./i);
+  const message2 = screen.getByText(/※これはテーマのサンプルです。/i);
 
-  // Ensure there are 2 instances of original title and message
-  expect(originalTitle.length).toEqual(2);
-  expect(originalMessage.length).toEqual(2);
+  expect(title).toBeInTheDocument();
+  expect(message1).toBeInTheDocument();
+  expect(message2).toBeInTheDocument();
 });
 
-test('renders new title and message', () => {
+test('renders with light theme', () => {
   render(<App />);
-  const newTitle = screen.getByText(/新しいタイトル/i);
-  const newMessage = screen.getByText(/これは新しいメッセージです。/i);
+  const title = screen.getByText(/Content page/i);
+  const message1 = screen.getByText(/This is Content sample./i);
+  const message2 = screen.getByText(/※これはテーマのサンプルです。/i);
 
-  expect(newTitle).toBeInTheDocument();
-  expect(newMessage).toBeInTheDocument();
-});
-
-test('checks total number of title and message', () => {
-  render(<App />);
-  const totalTitle = screen.getAllByText(/Title|新しいタイトル/i);
-  const totalMessage = screen.getAllByText(/this is sample message.|これは新しいメッセージです。/i);
-
-  // Ensure there are 3 instances of title and message in total
-  expect(totalTitle.length).toEqual(3);
-  expect(totalMessage.length).toEqual(3);
+  // Check the background color and text color to verify the light theme
+  expect(title).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
+  expect(message1).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
+  expect(message2).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
 });
