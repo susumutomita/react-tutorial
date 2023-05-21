8b1805c8f2f1b59e066d3ddcaef21bc16b8ec9ab
susumu tomita <oyster880@gmail.com>
2023-05-22 07:13:35 +0900

    add list 4-2

--- src/App.js
+++ src/App.js
@@ -1,52 +1,75 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 
-let theme = {
-  light: {
-    backgroundColor: "#eef",
-    color: "#006",
-    padding: "10px",
-  },
-  dark: {
-    backgroundColor: "#006",
-    color: "#eef",
-    padding: "10px",
-  }
-};
-
-const ThemeContext = React.createContext(theme.light);
+function mappingState(state) {
+  return state;
+}
 
 class App extends Component {
 
-  static contextType = ThemeContext;
-  render() {
-    return <div style={this.context}>
-      <Title value="Content page" />
-      <Message value="This is Content sample." />
-      <Message value="※これはテーマのサンプルです。" />
-    </div>;
+  constructor(props) {
+    super(props);
   }
-}
-
-class Title extends Component {
-  static contextType = ThemeContext;
-
-  render() {
-    return <div>
-      <h2 style={this.context}>{this.props.value}</h2>
-    </div>;
-  }
-}
-
-class Message extends Component {
-
-  static contextType = ThemeContext;
 
   render() {
     return (
-      <p style={this.context}>{this.props.value}</p>
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
+      </div>
     );
   }
 }
+
+App = connect()(App);
+
+
+class Message extends Component {
+
+  style = {
+    fontSize: "20pt",
+    padding: "20px 5px",
+  }
+
+  render() {
+    return (
+      <p style={this.context}>{this.props.message} : { this.props.counter}</p>
+    );
+  }
+}
+
+Message = connect(mappingState)(Message);
+
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px",
+  }
+
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type: "DECREMENT" });
+    } else {
+      this.props.dispatch({ type: "INCREMENT" });
+    }
+  }
+
+  render() {
+    return (
+      <button style={this.style} onClick={this.doAction}>
+        click
+      </button>
+    );
+  }
+}
+
+Button = connect()(Button);
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { render, fireEvent, screen } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import App from "./App";
 
-test('renders title and messages', () => {
-  render(<App />);
-  const title = screen.getByText(/Content page/i);
-  const message1 = screen.getByText(/This is Content sample./i);
-  const message2 = screen.getByText(/※これはテーマのサンプルです。/i);
+const mockStore = configureStore([]);
+let store;
 
-  expect(title).toBeInTheDocument();
-  expect(message1).toBeInTheDocument();
-  expect(message2).toBeInTheDocument();
-});
+describe("App Component", () => {
+  beforeEach(() => {
+    store = mockStore({
+      counter: 0,
+      message: "COUNTER",
+    });
 
-test('renders with light theme', () => {
-  render(<App />);
-  const title = screen.getByText(/Content page/i);
-  const message1 = screen.getByText(/This is Content sample./i);
-  const message2 = screen.getByText(/※これはテーマのサンプルです。/i);
+    store.dispatch = jest.fn();
 
-  // Check the background color and text color to verify the light theme
-  expect(title).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
-  expect(message1).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
-  expect(message2).toHaveStyle({ backgroundColor: '#eef', color: '#006' });
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+
+  it("should render with given state from Redux store", () => {
+    expect(screen.getByText(/COUNTER/)).toBeInTheDocument();
+    expect(screen.getByText(/0/)).toBeInTheDocument();
+
+  });
+
+  it("should dispatch an action on button click", () => {
+    fireEvent.click(screen.getByText("click"));
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith({ type: "INCREMENT" });
+  });
+
+  it("should dispatch an action on button click with shift key", () => {
+    fireEvent.click(screen.getByText("click"), { shiftKey: true });
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith({ type: "DECREMENT" });
+  });
 });
