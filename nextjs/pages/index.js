e3a1957b62b6f771c0c5dfa630e30d1f4e2d28e4
susumu tomita <oyster880@gmail.com>
2023-06-02 07:50:06 +0900

    add list 5-7

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,19 +1,21 @@
-const h1 = {
-  fontSize: "72pt",
-  fontWeight: "bold",
-  textAlign: "right",
-  letterSpacing: "-8px",
-  color: "#f0f0f0",
-  margin: "-40px 0px",
-};
-
-const p = {
-  margin: "0px",
-  color: "#666",
-  fontSize: "16pt",
-};
-
-export default () => <div>
-  <h1 style={h1}>Next.js</h1>
-  <p style={p}>Welcome to next.js!</p>
-</div>
+export default () => (
+  <div>
+    <style jsx>{`
+      h1 {
+        font-size: 72pt;
+        font-weight: bold;
+        text-align: right;
+        letter-spacing: -8px;
+        color: #f0f0f0;
+        margin: -40px 0px;
+      }
+      p {
+        margin: 0px;
+        color: blue;
+        font-size: 16pt;
+      }
+    `}</style>
+    <h1>Next.js</h1>
+    <p>this is built in style</p>
+  </div>
+)
