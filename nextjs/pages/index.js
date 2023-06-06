a9ef62f144cc75355f3df9a9fedcab6fc312e944
susumu tomita <oyster880@gmail.com>
2023-06-07 06:05:23 +0900

    add list 5-22

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Image from '../static/Image';
 
 export default () => (
   <Layout header="Next" title="Top page.">
     <p>Welcome to next.js!</p>
+    <Image fname="image.png" size="300"/>
     <hr />
     <Link href="./other">
       <button>Go to Other &gt;&gt;</button>
