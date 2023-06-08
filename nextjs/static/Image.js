cd73c5a87fc7766f07ebf5e4f81bd6bcb1c1abab
susumu tomita <oyster880@gmail.com>
2023-06-09 07:23:27 +0900

    add list 5-28

--- nextjs/static/Image.js
+++ nextjs/static/Image.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-
 class Image extends Component {
 
-constructor(props) {
-  super(props);
-  this.fname = './static/' + props.fname;
-  this.size = props.size + 'px';
-}
-
-  render() {
-    return (
-      <img width = {this.size} boorder="1" src={this.fname}/>
-    );
+  constructor(props) {
+    super(props);
+    this.fname = './static/' + props.fname;
+    this.size = props.size + 'px';
   }
-}
-
-export default Image;
+  
+    render() {
+      return (
+        <img width = {this.size} boorder="1" src={this.fname} alt="This is sample image"/>
+      );
+    }
+  }
+  
+  export default Image;
+  
