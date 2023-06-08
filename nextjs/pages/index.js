6803a7b1de123b9a0d4c5d47adf1aa22b26c5a3b
susumu tomita <oyster880@gmail.com>
2023-06-09 07:25:40 +0900

    add list 5-28

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -2,13 +2,15 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Counter from '../components/Counter';
 
-export default () => (
+const HomePage = () => (
   <Layout header="Next" title="Top page.">
-    <Counter />
     <p>Welcome to next.js!</p>
+    <Counter />
     <hr />
     <Link href="./other">
       <button>Go to Other &gt;&gt;</button>
     </Link>
   </Layout>
 )
+
+export default HomePage;
--- nextjs/pages/other.js
+++ nextjs/pages/other.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Counter from '../components/Counter';
 
-export default () => (
+const OtherPage = () => (
   <Layout header="Other" title="Other page.">
     <p>this is Other page.</p>
     <Counter />
@@ -14,3 +14,5 @@ export default () => (
     </div>
   </Layout>
 )
+
+export default OtherPage;
