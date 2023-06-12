8a9ad1cf3d17fc9a7b76440a26e1cc0e8be8fa7e
susumu tomita <oyster880@gmail.com>
2023-06-13 07:41:50 +0900

    add list 6-5

--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createStore } from "redux";
+// import { Provider } from 'react-redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import "./index.css";
+// import App from "./App";
+// import { memoReducer } from "./memo/Store";
+
+// const persistConfig = {
+//   key: 'memo',
+//   storage,
+//   blacklist: ['message', 'mode', 'fdata'],
+//   whitelist: ['data']
+// };
+
+// const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+// let store = createStore(persistedReducer);
+// let pstore = persistStore(store);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <PersistGate loading={<p>loading ...</p>}
+//       persistor={pstore}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+// export default pstore;
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
-import "./index.css";
+import './index.css';
 import App from "./App";
-import { memoReducer } from "./memo/Store";
-
-const persistConfig = {
-  key: 'memo',
-  storage,
-  blacklist: ['message', 'mode', 'fdata'],
-  whitelist: ['data']
-};
-
-const persistedReducer = persistReducer(persistConfig, memoReducer);
-
-let store = createStore(persistedReducer);
-let pstore = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={<p>loading ...</p>}
-      persistor={pstore}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <App />,
   document.getElementById("root")
 );
 
-export default pstore;
