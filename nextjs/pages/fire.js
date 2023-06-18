2428e0d2adaa8606ecbe8d8722c6aae86abb16f1
susumu tomita <oyster880@gmail.com>
2023-06-19 06:58:51 +0900

    add list 6-7

--- nextjs/pages/fire.js
+++ nextjs/pages/fire.js
@@ -3,10 +3,7 @@ import Layout from '../components/Layout';
 import Sampledata from '../components/Sampledata';
 
 export default () => (
-  <Layout header="Fire" title="fire">
-    <h1>Fire</h1>
-
-    <p>hogehoge</p>
+  <Layout header="Fire" title="Sample data.">
     <Sampledata />
-</Layout>
+  </Layout>
 );
